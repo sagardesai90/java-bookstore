6fa3cdec9f6a9a7bb93357f2a812b401eaf3299d
sagardesai90 <saagardesai@gmail.com>
2019-03-01 09:01:39 -0800

    First commit

---
+++ src/main/java/com/lambdaschool/javabookstore/JavaBookstoreApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.javabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookstoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavaBookstoreApplication.class, args);
+    }
+
+}
